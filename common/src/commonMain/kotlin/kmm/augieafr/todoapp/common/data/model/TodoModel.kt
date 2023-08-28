51b74be28253990781ec0bf09dae0a19731d0bf0
Augie Afriyansyah <augieafriyansyah@gmail.com>
2023-08-28 15:05:50 +0700

    refactor(database): add NOT NULL modifier to isDone and delete TodoModel as we use generated model from sqldelight

--- common/src/commonMain/kotlin/kmm/augieafr/todoapp/common/data/model/TodoModel.kt
+++
@@ -1,9 +0,0 @@
-package kmm.augieafr.todoapp.common.data.model
-
-data class TodoModel(
-    val id: Long,
-    val title: String,
-    val description: String,
-    val dueDate: String,
-    val isDone: Boolean,
-)
